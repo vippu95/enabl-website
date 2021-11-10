5d4f0e67c6059c1539a7b57d34fcdb25b45af3bb
FlutLab.io <>
2021-11-10 15:29:43 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         // you want
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'vipinsi home page'),
+      home: MyHomePage(title: 'Thsi is awsome'),
     );
   }
 }
