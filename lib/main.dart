1c8060bcc49d84586d1314995204eb13e26aef40
FlutLab.io <>
2021-11-10 15:28:15 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         // you want
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Clicker Counter Home Page'),
+      home: MyHomePage(title: 'vipinsi home page'),
     );
   }
 }
