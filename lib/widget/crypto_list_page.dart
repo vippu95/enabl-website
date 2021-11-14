51164650bebb6c810ac8bdd0d3ca40b53e903fdf
FlutLab.io <>
2021-11-14 08:41:55 +0000

    Autosave

--- lib/widget/crypto_list_page.dart
+++ lib/widget/crypto_list_page.dart
@@ -31,7 +31,7 @@ class CryptoListPage extends StatelessWidget {
   Widget _buildAppBar() {
     return AppBar(
       elevation: 0.1,
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.lightBlue,
       title: Text("Crypto Tracker"),
       actions: <Widget>[
         IconButton(
