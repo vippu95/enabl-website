67d9285621b58a251c6eb31b00a0a668c2a31e59
vippu95 <vippu95@gmail.com>
2021-11-14 07:40:30 +0000

    Temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:enabl/constant/custom_colors.dart';
 import 'package:enabl/data/simple_price_api_response.dart';
 import 'package:enabl/services/coingecko_crypto_api_service.dart';
-import 'package:enabl/widget/crypto_list_widget.dart';
+import 'package:enabl/widget/crypto_list_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -12,5 +12,5 @@ void main() async {
   runApp(new MaterialApp(
       title: 'Crypto Tracker',
       theme: new ThemeData(primaryColor: CustomColors.darkColor),
-      home: new CryptoListWidget(simplePriceResponse)));
+      home: new CryptoListPage(simplePriceResponse)));
 }
--- lib/widget/crypto_list_page.dart
+++ lib/widget/crypto_list_page.dart
@@ -1,17 +1,19 @@
+import 'package:enabl/constant/custom_colors.dart';
 import 'package:enabl/data/simple_price_api_response.dart';
 import 'package:enabl/widget/crypto_list_card.dart';
 import 'package:flutter/material.dart';
 
-class CryptoListWidget extends StatelessWidget {
+class CryptoListPage extends StatelessWidget {
   final SimplePriceResponse _tickerResponse;
 
-  CryptoListWidget(this._tickerResponse);
+  CryptoListPage(this._tickerResponse);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: CustomColors.darkColor,
+      appBar: _buildAppBar(),
       body: _buildBody(),
-      backgroundColor: Colors.blue,
     );
   }
 
@@ -21,5 +23,19 @@ class CryptoListWidget extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return CryptoListCard(_tickerResponse.entitiesList[index]);
         });
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      elevation: 0.1,
+      backgroundColor: CustomColors.darkColor,
+      title: Text("Crypto list"),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {},
+        )
+      ],
+    );
   }
 }
